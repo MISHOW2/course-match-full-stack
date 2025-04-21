bb3a742a3a71fc29dba28ce6dfd042637d0d8d69
MISHOW2 <mshibambo63@gmail.com>
2025-04-21 15:53:49 +0200

    updated db

--- server/config/db.js
+++ server/config/db.js
@@ -5,4 +5,39 @@ const users = [
   }
 ]
 
+
+
+const courses = [
+  {
+    id: 1,
+    name: "Nursing",
+    minAps: 26,
+    field: "Health Sciences"
+  },
+  {
+    id: 2,
+    name: "Information Technology",
+    minAps: 28,
+    field: "Computer Science"
+  },
+  {
+    id: 3,
+    name: "Education",
+    minAps: 22,
+    field: "Teaching"
+  },
+  {
+    id: 4,
+    name: "Mechanical Engineering",
+    minAps: 30,
+    field: "Engineering"
+  },
+  {
+    id: 5,
+    name: "Business Management",
+    minAps: 24,
+    field: "Commerce"
+  }
+];
+
 module.exports = users
